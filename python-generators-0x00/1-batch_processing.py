7f4dfef60770b937d56e08193ad58627b43f0dea
kal576 <cmuuo5864@gmail.com>
2025-05-16 14:54:04 +0300

    Add filter function to print users >25

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -33,7 +33,15 @@ def stream_users_in_batches(batch_size):
             connection.close()
 
 def batch_processing(batch_size):
+
+    filtered_users = []
+
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age']>25:
-                print(user)
+                filtered_users.append(user)
+    
+    for user in filtered_users:
+        print(user)
+    
+    return filtered_users
