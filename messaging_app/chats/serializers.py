f70fe2f81254a7dcf737ce4c229b05cd1ecbfe58
kal576 <cmuuo5864@gmail.com>
2025-06-03 10:18:03 +0300

    Add validation to UserSerializers module

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -7,9 +7,20 @@ or other content types an vice versa
 """
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length = 50)
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'bio', 'created_at']
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+    
+    def validate_username(self, value):
+        if "admin" in value.lower():
+            raise serializers.ValidationError("User cannot contain 'admin")
+        return value
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
