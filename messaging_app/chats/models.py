0f6434128199981c9e07f671f7b25256867b11dc
kal576 <cmuuo5864@gmail.com>
2025-06-03 10:02:29 +0300

    Add password to User model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -18,6 +18,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=15)
+    password = models.CharField(max_length=20)
     bio = models.TextField()
     created_at = models.DateTimeField(
         default= timezone.now
