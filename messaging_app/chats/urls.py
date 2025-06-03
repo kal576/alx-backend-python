add02217898f12d24edcbdb7d02225f1a3a94041
kal576 <cmuuo5864@gmail.com>
2025-06-03 11:46:02 +0300

    Add urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+router = NestedDefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
-router.register(r'messages', MessageViewSet, basename='messages')
 
-urlpatterns = router.urls [
-    path('', include(router.urls))
+convo_router = NestedDefaultRouter(router, r'conversations', lookup='conversaton')
+convo_router.register(r'messages', MessageViewSet, basename='conversation_name')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(convo_router.urls))
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('chats.urls')),
+    path('api-auth/', include('chats.urls')),
 ]
