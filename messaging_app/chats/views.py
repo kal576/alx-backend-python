f4287e405c56d178ce604c1e99ee1cee9117e9f0
kal576 <cmuuo5864@gmail.com>
2025-06-03 11:04:48 +0300

    Add views.py an urls.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class  = ConversationSerializer
+    permission_classes = [permissions.IsAuthenitacted]
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class  = MessageSerializer
+    permission_classes = [permissions.IsAuthenitacted]
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
