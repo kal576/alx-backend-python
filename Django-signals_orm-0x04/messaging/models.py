0a101f89881276765237493f748d935dda5e71ec
kal576 <cmuuo5864@gmail.com>
2025-06-12 20:18:04 +0300

    add unread field

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-from .manager import UnreadMessagesManager
+from .managers import UnreadMessagesManager
 
 class Message(models.Model):
     message_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
