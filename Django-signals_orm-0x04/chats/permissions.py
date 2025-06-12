29fbe7964ee117dfe68fde1d1172e2faf90118b7
kal576 <cmuuo5864@gmail.com>
2025-06-12 14:31:39 +0300

    add models.py and signals.py

---
+++ Django-signals_orm-0x04/chats/permissions.py
@@ -0,0 +1,27 @@
+from rest_framework import permissions
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    allow only authenticated users
+    allow access only if the user is a participant of the conversation.
+    """
+    
+    SAFE_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+
+    def has_permission(self, request, view):
+        # Check if the user is authenticated
+        return request.user and request.user.is_authenticated
+    
+
+    def has_object_permission(self, request, view, obj):
+        # Check if the user is a participant in the conversation
+        if request.method not in permissions.SAFE_METHODS:
+            return False
+
+        user = request.user
+        if hasattr(obj, 'participants'):
+            return user in obj.participants.all()
+        if hasattr(obj, 'conversation'):
+            return user in obj.conversation.participants.all()
+        return False
