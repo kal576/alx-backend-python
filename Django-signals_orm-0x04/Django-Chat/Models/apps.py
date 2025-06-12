52cdab867a0c08764c3514d1aa8b360b44bea01b
kal576 <cmuuo5864@gmail.com>
2025-06-12 17:48:27 +0300

    add chat logging

---
+++ Django-signals_orm-0x04/Django-Chat/Models/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class ModelsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Django-Chat.Models'
+
+    def ready(self):
+        import Django_Chat.Models.signals
