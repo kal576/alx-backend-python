41dc4906f439f87bbc42eb025cfe17daa1f72616
kal576 <cmuuo5864@gmail.com>
2025-06-21 21:20:58 +0300

    Add docker-composer.yml

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -122,6 +122,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': 'db',  # This matches the service name in docker-compose.yml
+        'PORT': '3306'
     }
 }
 
